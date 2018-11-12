b80bc61202b101029887791ea46eeee10eedbeb5
Henrique Caúla <lhtc@cin.ufpe.br>
2018-11-12 17:30:02 -0300

    Gets gravity change direction based on camera direction

--- Assets/Scripts/GlobalGravityController.cs
+++ Assets/Scripts/GlobalGravityController.cs
@@ -2,59 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalGravityController : MonoBehaviour {
-	
-	public Vector3 gravityDirection;
-	public float gravityForce;
+public class GlobalGravityController : MonoBehaviour
+{
 
-	private Vector3 forward, right, up;
-	private SwipeController sc;
+    public Vector3 gravityDirection;
+    public float gravityForce;
 
-	void Start()
-	{
-		sc = GameObject.Find("Scene Manager").gameObject.GetComponent<SwipeController>();
-	}
+    private Vector3 forward, right, up;
+    private SwipeController sc;
 
-	void Update()
-	{
-		// /* Getting the camera's forward direction vector */
-		forward = Camera.main.transform.forward;
+    void Start()
+    {
+        sc = GameObject.Find("Scene Manager").gameObject.GetComponent<SwipeController>();
+    }
 
-		// /* Forcing the y vector direction as 0 */
-		// forward.y = 0;
+    void Update()
+    {
 
-		// /* Normalizing forward direction for padronization */
-		// forward = Vector3.Normalize(forward);
+        // /* Getting the camera's forward direction vector */
+        // forward = Camera.main.transform.forward;
 
-		// /* Setting the right vector based on the forward direction */
-		// right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+        // /* Forcing the y vector direction as 0 */
+        // forward.y = 0;
 
-		// Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
-		// Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+        // /* Normalizing forward direction for padronization */
+        // forward = Vector3.Normalize(forward);
 
-		// Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        // /* Setting the right vector based on the forward direction */
+        // right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
 
-		// transform.forward = heading;
-		// transform.position += rightMovement;
-		// transform.position += upMovement;
+        // Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        // Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
 
-		if (sc.SwipeUp) 
-		{
-			print(forward);
-			ChangeGravity("up", null);
-		}
-		else if (sc.SwipeDown) ChangeGravity("down", null);
-	}
+        // Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
 
-	void ChangeGravity(string vertical, string horizontal)
-	{
-		if (vertical == "up")
-		{
-			if (string.IsNullOrEmpty(horizontal)) gravityDirection = Vector3.up;
-		} 
-		else
-		{
-			if (string.IsNullOrEmpty(horizontal)) gravityDirection = Vector3.down;
-		}
-	}
+        // transform.forward = heading;
+        // transform.position += rightMovement;
+        // transform.position += upMovement;
+
+        forward = Camera.main.transform.forward;
+        right = Camera.main.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        Vector3 upRight = Vector3.Normalize(forward + right);
+        Vector3 downLeft = Vector3.Normalize(upRight * -1);
+        Vector3 upLeft = Vector3.Normalize(forward - right);
+        Vector3 downRight = Vector3.Normalize(upLeft * -1);
+
+        if (sc.SwipeUp) ChangeGravity("up", null);
+        else if (sc.SwipeDown) ChangeGravity("down", null);
+		else if (sc.SwipeUpLeft) print(upLeft);
+		else if (sc.SwipeUpRight) print(upRight);
+		else if (sc.SwipeDownLeft) print(downLeft);
+		else if (sc.SwipeDownRight) print(downRight);
+    }
+
+    void ChangeGravity(string vertical, string horizontal)
+    {
+        if (vertical == "up")
+        {
+            if (string.IsNullOrEmpty(horizontal)) gravityDirection = Vector3.up;
+        }
+        else
+        {
+            if (string.IsNullOrEmpty(horizontal)) gravityDirection = Vector3.down;
+        }
+    }
 }
