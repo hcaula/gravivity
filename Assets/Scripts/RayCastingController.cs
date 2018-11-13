add00f06c333d20142bfda699e060874fa13a7ba
Henrique Caúla <lhtc@cin.ufpe.br>
2018-11-13 02:15:55 -0300

    Makes transparent if center stage is behind wall

---
+++ Assets/Scripts/RayCastingController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastingController : MonoBehaviour {
+
+	public GameObject[] targetedPlatforms;
+	
+	void Update () {
+		Vector3 center = GameObject.FindGameObjectsWithTag("Center")[0].gameObject.transform.position;
+		Vector3 camera = transform.position;
+		Vector3 castDirection = center - camera;
+		float distance = Vector3.Distance(camera, center);
+
+		Ray landingRay = new Ray(camera, castDirection);
+
+		RaycastHit hit;
+		if (Physics.Raycast(landingRay, out hit, distance))
+		{
+			GameObject targetHit = hit.collider.gameObject;
+			for (int i = 0; i < targetedPlatforms.Length; i++) 
+			{
+				GameObject platform = targetedPlatforms[i].gameObject;
+				bool isOnView = false;
+				if (targetHit != null && GameObject.ReferenceEquals(platform, targetHit)) isOnView = true;
+				platform.GetComponent<TransparentController>().SetIsOnView(isOnView);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TransparentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentController : MonoBehaviour
+{
+
+    #region Private attributes
+    private Animator animator;
+    #endregion
+
+    public void SetIsOnView(bool value) { 
+		if (value) print("Hi!");
+		animator.SetBool("IsOnView", value); 
+	}
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+	}
+}
