63de99e4228b2f8ebe52e82b80f3816c8d802a62
Henrique Caúla <lhtc@cin.ufpe.br>
2018-11-12 22:24:47 -0300

    Comments on Camera Controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
+	#region Public attributes
 	public int rotationSpeed;
+	#endregion
+
+	#region Private attributes
 	private bool isRotating;
 	private DoubleTapController dtc;
+	#endregion
 
 	void Start()
 	{
+		/* Start this boolean as false */
 		isRotating = false;
+
+		/* Gets the Double Tab Controller component */
 		dtc = GameObject.Find("Scene Manager").gameObject.GetComponent<DoubleTapController>();
 	}
 
     void Update()
     {
+		/* If the camera isn't in mid-rotation */
 		if (!isRotating)
 		{
 			if (dtc.DtRight || Input.GetKeyDown("d")) Rotate("right");
@@ -28,15 +37,21 @@ public class CameraController : MonoBehaviour
     {
 		isRotating = true;
 
+		/* Gets the camera's current Y-Axis value */
 		int currentY = Mathf.RoundToInt(transform.rotation.eulerAngles.y);
+
+		/* Adds or decreases 90 degrees to it */
 		int targetY = direction == "left" ? currentY + 90 : currentY - 90;
 
+		/* Calls rotation animation routine */
 		StartCoroutine(AnimateRotation(currentY, targetY));
     }
 
 	IEnumerator AnimateRotation(int currentY, int targetY)
 	{
+		/* Calculates the camera rotation step speed */
 		int step = currentY > targetY ? -rotationSpeed : rotationSpeed;
+
 		while (currentY != targetY)
 		{
 			yield return new WaitForSeconds(0.01f);
@@ -44,6 +59,7 @@ public class CameraController : MonoBehaviour
 			transform.rotation = Quaternion.Euler(30, currentY, 0);
 		}
 
+		/* After animation is over, set rotating boolean as false */
 		isRotating = false;
 	}
 }
