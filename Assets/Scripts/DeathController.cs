247ccdc09a0fa625d5d8e3d877a6137c38d49c8f
Henrique Caúla <lhtc@cin.ufpe.br>
2018-11-16 01:33:17 -0300

    Starts developing death animation

---
+++ Assets/Scripts/DeathController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathController : MonoBehaviour
+{
+
+    public int fragmentAmount;
+    public bool killIt = false;
+    private bool isDying = false;
+	private Vector3 originalPosition;
+
+    public GameObject fragmentPrefab;
+	public float fragmentSpeed;
+
+	void Start()
+	{
+		originalPosition = transform.position;
+	}
+
+    void Update()
+    {
+        if (killIt && !isDying) Die();
+    }
+
+    void Die()
+    {
+        isDying = true;
+		AnimateFragments();
+    }
+
+	void AnimateFragments()
+	{
+		GetComponent<MeshRenderer>().enabled = false;
+		int min = -10, max = 10;
+
+		GameObject[] fragments = new GameObject[fragmentAmount];
+
+		for (int i = 0; i < fragmentAmount; i++)
+        {
+			float y = Random.Range(min, max);
+			float x = Random.Range(min, max);
+			float z = Random.Range(min, max);
+
+            GameObject fragment = Instantiate(fragmentPrefab, this.transform);
+
+			fragment.GetComponent<Rigidbody>().AddForce(new Vector3(x, y ,z) * fragmentSpeed);
+
+			fragments[i] = fragment;
+        }
+	}
+}
