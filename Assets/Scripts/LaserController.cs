f810e8d2b855c08fd293613df56f25f9af86d354
Henrique Caúla <lhtc@cin.ufpe.br>
2018-11-15 22:15:19 -0300

    Adds laser interaction with button

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -10,31 +10,37 @@ public class LaserController : MonoBehaviour {
 	#region Private attributes
     private ButtonInteractionController bic;
 	private LineRenderer lr;
+	private GameObject beam;
 	#endregion
 
     void Start()
     {
         bic = GetComponent<ButtonInteractionController>();
-		lr = GetComponent<LineRenderer>();
+
+		beam = transform.GetChild(2).gameObject;
+		lr = beam.GetComponent<LineRenderer>();
     }
 
     void Update()
     {
 		/* If this laser is linked to a button, check if it's pressed before activating */
 		/* If it's not linked, active always */
-        if ((bic && bic.GetIsActive()) || !bic) ActivateBeam();
+		if (bic && bic.GetIsActive()) lr.enabled = true;
+		else lr.enabled = false;
+
+		/* Renders the laser */
+		if (lr.enabled) CalculateBeam();
     }
 
-    void ActivateBeam()
-    {
-		lr.SetPosition(0, this.transform.position);
+	void CalculateBeam()
+	{
+		lr.SetPosition(0, beam.transform.position);
 		RaycastHit hit;
-		if (Physics.Raycast(this.transform.position, transform.forward, out hit))
+		if (Physics.Raycast(beam.transform.position, beam.transform.forward, out hit))
 		{
-			if (hit.collider)
-			{
-				lr.SetPosition(1, hit.point);
-			}
-		} else lr.SetPosition(1, this.transform.forward * 5000);
-    }
+			/* If the raycast hits, laser ends where it hit */
+			if (hit.collider) lr.SetPosition(1, hit.point);
+			
+		} else lr.SetPosition(1, beam.transform.forward * 5000);
+	}
 }
