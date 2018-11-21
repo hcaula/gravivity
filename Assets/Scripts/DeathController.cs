d413ae46aef4309336f31ad4f5d9c544446645a3
Arthurlpgc <arthurlpgcosta@gmail.com>
2018-11-21 00:11:08 -0300

    death improvements- original postion still wrong, needs smoothing on move section

--- Assets/Scripts/DeathController.cs
+++ Assets/Scripts/DeathController.cs
@@ -8,7 +8,16 @@ public class DeathController : MonoBehaviour
     public int fragmentAmount;
     public bool killIt = false;
     private bool isDying = false;
-	private Vector3 originalPosition;
+    private int deathCounter = 300;
+
+    private int deathState;
+    private float deathTime = 0;
+    public float boomDuration = 1;
+    public float moveDuration = 1;
+
+    private GameObject[] fragments = null;
+    private Vector3[] forces;
+    private Vector3 originalPosition;
 
     public GameObject fragmentPrefab;
 	public float fragmentSpeed;
@@ -20,31 +29,56 @@ public class DeathController : MonoBehaviour
 
     void Update()
     {
-        if (killIt && !isDying) Die();
+        deathCounter--;
+        if (killIt && !isDying && deathCounter == 0) Die();
+        if (isDying) {
+            if (deathState == 1 && deathTime < moveDuration) RedirectFragments();
+            if (deathTime < 0)
+            {
+                isDying = false;
+                for (int i = 0; i < fragmentAmount * fragmentAmount * fragmentAmount; i++) fragments[i].GetComponent<Rigidbody>().velocity=new Vector3(0,0,0);
+            }
+            deathTime -= Time.deltaTime;
+
+        }
     }
 
     void Die()
     {
         isDying = true;
+        deathTime = moveDuration + boomDuration;
+        deathState = 1;
 		AnimateFragments();
+    }
+
+    void RedirectFragments() {
+        for (int i = 0; i < fragmentAmount * fragmentAmount * fragmentAmount; i++)
+        {
+            fragments[i].GetComponent<Rigidbody>().AddForce(-forces[i]);
+            Vector3 delta = originalPosition - fragments[i].GetComponent<Rigidbody>().position;
+            fragments[i].GetComponent<Rigidbody>().AddForce(delta / deathTime, ForceMode.VelocityChange);
+        }
+        deathState = 2;
     }
 
 	void AnimateFragments()
 	{
 		GetComponent<MeshRenderer>().enabled = false;
-		int min = -10, max = 10;
 
-		GameObject[] fragments = new GameObject[fragmentAmount];
+		fragments = new GameObject[fragmentAmount * fragmentAmount * fragmentAmount];
+        forces = new Vector3[fragmentAmount * fragmentAmount * fragmentAmount];
 
-		for (int i = 0; i < fragmentAmount; i++)
+        for (int i = 0; i < fragmentAmount * fragmentAmount * fragmentAmount; i++)
         {
-			float y = Random.Range(min, max);
-			float x = Random.Range(min, max);
-			float z = Random.Range(min, max);
+            float y = ((i/(fragmentAmount * fragmentAmount)) - fragmentAmount / 2) + Random.Range(-1.0f/fragmentAmount, 1.0f / fragmentAmount);
+            float x = ((i / fragmentAmount) % fragmentAmount - fragmentAmount / 2) + Random.Range(-1.0f / fragmentAmount, 1.0f / fragmentAmount);
+			float z = (i % fragmentAmount - fragmentAmount / 2) + Random.Range(-1.0f / fragmentAmount, 1.0f / fragmentAmount);
 
             GameObject fragment = Instantiate(fragmentPrefab, this.transform);
 
-			fragment.GetComponent<Rigidbody>().AddForce(new Vector3(x, y ,z) * fragmentSpeed);
+            forces[i] = new Vector3(x, y, z) * fragmentSpeed;
+
+            fragment.GetComponent<Rigidbody>().AddForce(forces[i]);
 
 			fragments[i] = fragment;
         }
