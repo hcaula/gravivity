3bebc0e7662ebc22cd167e050c11e4b1421ddc63
bruno <bfl2@cin.ufpe.br>
2018-12-09 19:23:47 -0300

    Added level pass methods. Now the next level is loaded when the player collides with the Hole.

---
+++ Assets/Scripts/HoleControler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleControler : MonoBehaviour {
+
+    private SceneManager sm;
+    public bool loadNextLevel = true;
+    public string sceneToLoad;
+	// Use this for initialization
+	void Start () {
+        sm = FindObjectOfType<SceneManager>();
+     
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (sm == null)
+        {
+            Debug.Log("Scene Manager not found.");
+        }
+        else
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                if (loadNextLevel)
+                {
+                    sm.LoadNextScene();
+                }
+                else
+                {
+                    sm.LoadSceneByName(sceneToLoad);
+                }
+            }
+        }
+    }
+
+
+}
+
+
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManager : MonoBehaviour {
+
+    public void LoadSceneByName(string name)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(name);
+    }
+    public void LoadNextScene()
+    {
+        Scene current = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+        Debug.Log("Current Scene: "+ current.buildIndex + " Next Scene: "+ (current.buildIndex+1));
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(current.buildIndex + 1);
+    }
+}
