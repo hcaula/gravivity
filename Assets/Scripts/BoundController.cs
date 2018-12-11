2df88b90f192f73e5f2c293cc143c894c466b419
Henrique Caúla <lhtc@cin.ufpe.br>
2018-12-11 03:53:07 -0300

    Some scene adjustments

--- Assets/Scripts/BoundController.cs
+++ Assets/Scripts/BoundController.cs
@@ -6,10 +6,8 @@ public class BoundController : MonoBehaviour {
 
 	void OnCollisionEnter(Collision col)
 	{
-		print("hello!1");
 		if (col.gameObject.tag == "Player")
 		{
-			print("hello!2");
 			DeathController dc = col.gameObject.GetComponent<DeathController>();
             dc.Die();
 		}
